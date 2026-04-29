2f8f238085eae5c6877709f50bc4b340ae6320d9
adnanalbarudi <adnanalbarudi@gmail.com>
2026-04-29 22:39:52 +0300

    Update prog.c

--- prog.c
+++ prog.c
@@ -1,102 +1,85 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
-#define size_Buffer 9
-
-struct CircularBuffer
-{
-    char buffer[size_Buffer];
-    int head;
-    int tail;
-    int count;
+struct CB_Struct {
+    char buf[10]; 
+    int head; // Head
+    int tail; // Tail
+    int count; // Count 
 };
-
-    //itialize buffer
-void initialize_buffer(struct CircularBuffer *cb)
-{
+//تهيئة
+void init_cb(struct CB_Struct *cb) {
     cb->head = 0;
     cb->tail = 0;
     cb->count = 0;
 }
-
-    // check if buffer full or no
-bool is_buffer_full(struct CircularBuffer *cb)
-{
-    if (cb->count == size_Buffer)
-        return true;
-    else
-    return false;
+//فحص امتلاء
+int isFull(struct CB_Struct *cb) {
+    if (cb->count == 10) return 1; 
+    return 0;
 }
-
-    // Check if buffer empty or no
-bool is_buffer_empty(struct CircularBuffer *cb)
-{
-    if (cb->count == 0)
-        return true;
-    else
-        return false;
+//فحص فراغ  
+int isEmpty(struct CB_Struct *cb) {
+    if (cb->count == 0) return 1; 
+    return 0;
 }
-
-    // write character in buffer
-void write_in_buffer(struct CircularBuffer *cb, char data)
-{
-    if (is_buffer_full(cb))
-    {
-        printf("Buffer Overflow\n");
-        return;
+void put_char(struct CB_Struct *cb, char d) {
+    if (isFull(cb) == 1) {
+        printf("\nBuffer Overflow! lost: %c", d);
+    } else {
+        cb->buf[cb->tail] = d;
+        cb->tail = cb->tail + 1;
+        if (cb->tail == 10) cb->tail = 0; 
+        cb->count = cb->count + 1;
     }
-    cb->buffer[cb->tail] = data;
-    cb->tail = (cb->tail + 1) % size_Buffer;
-    cb->count++;
 }
 
-    // read character from buffer
-char read_from_buffer(struct CircularBuffer *cb)
-{
-    char data;
-    if (is_buffer_empty(cb))
-    {
-        printf("Buffer Underflow\n");
+char get_char(struct CB_Struct *cb) {
+    if (isEmpty(cb) == 1) {
+        printf("\nBuffer Underflow!");
         return '\0';
+    } else {
+        char d = cb->buf[cb->head];
+        cb->head = cb->head + 1;
+        if (cb->head == 10) cb->head = 0; 
+        cb->count = cb->count - 1;
+        return d;
     }
-    data = cb->buffer[cb->head];
-    cb->head = (cb->head + 1) % size_Buffer;
-    cb->count--;
-
-    return data;
 }
 
-int main()
-{
-    struct CircularBuffer cb;
-    char name[20];
-    char fulltext[50];
+int main() {
+    struct CB_Struct my_cb;
+    init_cb(&my_cb);
 
-    initialize_buffer(&cb);
+    char username[100];
+    printf("Enter the name: ");
+    scanf("%s", username);
 
-    printf("Enter your name: ");
-    scanf("%s", name);
+    int i = 0;
+    while (username[i] != '\0') i++;
+    
+  
+    username[i] = 'C';
+    username[i+1] = 'E';
+    username[i+2] = '-';
+    username[i+3] = 'E';
+    username[i+4] = 'S';
+    username[i+5] = 'Y';
+    username[i+6] = '\0';
 
-    strcpy(fulltext, name);
-    strcat(fulltext, "CE-ESY");
+    printf("the string: %s\n", username);
 
-    for (int i = 0; i < strlen(fulltext); i++)
-    {
-        write_in_buffer(&cb, fulltext[i]);
+    int j = 0;
+    while (username[j] != '\0') {
+        put_char(&my_cb, username[j]);
+        j++;
     }
 
-    printf("output: ");
-
-    while (!is_buffer_empty(&cb))
-    {
-         printf("%c", read_from_buffer(&cb));
+    printf("Output: ");
+    while (isEmpty(&my_cb) == 0) {
+        char out = get_char(&my_cb); 
+        if (out != '\0') printf("%c", out);
     }
 
-    printf("\n");
-
-    if (is_buffer_empty(&cb))
-        printf("Buffer is empty\n");
-
+    printf("\n count in buffer: %d\n", my_cb.count);
     return 0;
 }
